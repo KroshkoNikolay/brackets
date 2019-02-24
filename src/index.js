6f1ede6f088d0e0d76e29887b405909eb9d5034a
KroshkoNikolay <kolia_kroshko@ukr.net>
2019-02-24 21:41:59 +0200

    feat: add my solution to check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let indexArray = [];
+  for (let charIndex in str) {
+    const char = str[charIndex];
+    if (indexArray.length > 0 && char === bracketsConfig[indexArray[indexArray.length - 1]][1]) {
+      indexArray.pop();
+      continue;
+    }
+    const index = checkIsOpenBracket(char, bracketsConfig);
+    if (index >= 0) {
+      indexArray.push(index);
+    } else {
+      return false;
+    }
+  }
+  return indexArray.length === 0;
+}
+
+function checkIsOpenBracket(char, bracketsConfig) {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (char === bracketsConfig[i][0]){
+      return i;
+    } 
+  }
+  return -1;
 }
